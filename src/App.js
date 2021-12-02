5e764eb4333157b742b3593b41d1c3b38524c619
khw970421 <khw970421@kakao.com>
2021-12-02 15:11:36 +0900

    🎨 add : needit 폴더 구조 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from "@/logo.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  Login,
+  Register,
+  Center,
+  Member,
+  Username,
+  Message,
+  Schedule,
+  Notify,
+  Writes,
+} from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/center" element={<Center />} />
+        <Route path="/member" element={<Member />} />
+        <Route path="/username" element={<Username />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/notify" element={<Notify />} />
+        <Route path="/writes" element={<Writes />} />
+      </Routes>
+      <br />
+      <Link to="login">login</Link>
+      <br />
+      <Link to="register">register</Link>
+      <br />
+      <Link to="center">center</Link>
+      <br />
+      <Link to="member">member</Link>
+      <br />
+      <Link to="username">usename</Link>
+      <br />
+      <Link to="message">message</Link>
+      <br />
+      <Link to="schedule">schedule</Link>
+      <br />
+      <Link to="notify">notify</Link>
+      <br />
+      <Link to="writes">writes</Link>
+    </BrowserRouter>
   );
 }
 
