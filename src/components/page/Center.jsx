4316bb0999cab436a1ad9d9f5798491bc1b6386f
gyulhana <matearz@korea.ac.kr>
2021-12-07 00:56:16 +0900

    temp

--- src/components/page/Center.jsx
+++ src/components/page/Center.jsx
@@ -1,5 +1,15 @@
+import Header from "../Header";
+
 const Center = () => {
-  return <div>Center</div>;
+  return (
+    <div>
+      <Header type="main" />
+      <Header type="member" view="멤버" />
+      <Header type="member" view="센터" />
+      <Header type="edit" url="https://naver.com" />
+      <Header type="confirm" url="https://naver.com" />;
+    </div>
+  );
 };
 
 export default Center;
