8973ca4eabe3d9cfa80b30d3af91d90be35d8c36
gyulhana <matearz@korea.ac.kr>
2021-12-09 16:09:09 +0900

    🔧 fix: center.jsx 파일 수정 복구

--- src/components/page/Center.jsx
+++ src/components/page/Center.jsx
@@ -1,15 +1,5 @@
-import Header from "../Header";
-
 const Center = () => {
-  return (
-    <div>
-      <Header type="main" />
-      <Header type="member" view="멤버" />
-      <Header type="member" view="센터" />
-      <Header type="edit" url="https://naver.com" />
-      <Header type="confirm" url="https://naver.com" />;
-    </div>
-  );
+  return <div>Center</div>;
 };
 
 export default Center;
