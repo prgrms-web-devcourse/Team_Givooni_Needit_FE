1f31f25ac8f9dcc29a4ef41e37255466449489fb
alajillo <alajillo2021@gmail.com>
2021-12-11 19:29:23 +0900

    ✨  feat : notify 페이지 초안

--- src/components/domain/Notify/Notify.jsx
+++ src/components/domain/Notify/Notify.jsx
@@ -1,5 +1,51 @@
-const Notify = () => {
-  return <div>Notify</div>;
+import { useState } from "react";
+import { ListItemButton } from "@mui/material";
+import { List } from "@mui/material";
+import ListItemText from "@mui/material/ListItemText";
+import { ListItem } from "@mui/material";
+import Proptypes from "prop-types";
+const Notify = ({ list = [], onClick }) => {
+  const [hoverId, setHoverId] = useState("");
+  return (
+    <>
+      <List>
+        {list.map((post) => (
+          <ListItem key={post.id}>
+            <ListItemButton
+              alignItems="flex-start"
+              onClick={() => {
+                onClick(post.id);
+              }}
+              onMouseEnter={() => setHoverId(post.id)}
+            >
+              <List>
+                <ListItemText
+                  primary={post.title}
+                  sx={{
+                    color: "primary.main",
+                    typography: "h3",
+                  }}
+                ></ListItemText>
+                <ListItemText
+                  primary={post.text}
+                  sx={{
+                    color: `${
+                      hoverId === post.id ? "primary.main" : "gray_dark.dark"
+                    }`,
+                    typography: "body1",
+                  }}
+                ></ListItemText>
+              </List>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
 };
 
+Notify.propTypes = {
+  list: Proptypes.array,
+  onClick: Proptypes.func,
+};
 export default Notify;
--- src/pages/NotifyPage.jsx
+++ src/pages/NotifyPage.jsx
@@ -1,6 +1,38 @@
 import Notify from "@/components/domain/Notify/Notify";
+const dummyData = [
+  {
+    id: 1,
+    title: "새로운메세지",
+    text: "새로운 메세지가 도착했습니다. 확인해주세요",
+  },
+  {
+    id: 2,
+    title: "새로운메세지",
+    text: "새로운 메세지가 도착했습니다. 확인해주세요",
+  },
+  {
+    id: 3,
+    title: "새로운메세지",
+    text: "새로운 메세지가 도착했습니다. 확인해주세요",
+  },
+  {
+    id: 4,
+    title: "새로운메세지",
+    text: "새로운 메세지가 도착했습니다. 확인해주세요",
+  },
+  {
+    id: 5,
+    title: "새로운메세지",
+    text: "새로운 메세지가 도착했습니다. 확인해주세요",
+  },
+  {
+    id: 6,
+    title: "새로운메세지",
+    text: "새로운 메세지가 도착했습니다. 확인해주세요",
+  },
+];
 const NotifyPage = () => {
-  return <Notify />;
+  return <Notify list={dummyData} onClick={(id) => console.log(id)} />;
 };
 
 export default NotifyPage;
