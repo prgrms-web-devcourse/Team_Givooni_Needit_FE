e462499fe3ac6940b76d36737bc6f568db52ba4b
gyulhana <matearz@korea.ac.kr>
2021-12-13 23:46:31 +0900

    ✨ feat: schedule page 초안 구현

--- src/components/domain/Schedule/Schedule.jsx
+++ src/components/domain/Schedule/Schedule.jsx
@@ -1,5 +1,82 @@
+import Header from "@/components/base/Header";
+import BaseButton from "@/components/base/BaseButton";
+import Nav from "@/components/base/Nav";
+import styled from "styled-components";
+
 const Schedule = () => {
-  return <div>Schedule</div>;
+  return (
+    <>
+      <Header type="searchOut" fixed={true} />
+      <AlignContainer>
+        <CardContainer>
+          {DUMMY_DATA.map((data) => {
+            return (
+              <>
+                <div key={data.contractId}>{data.contractDate}</div>
+                <BaseButton
+                  height="2rem"
+                  width="6rem"
+                  text={data.status === "done" ? "기부 완료" : "기부 진행"}
+                />
+                {data.status === "done" && (
+                  <BaseButton
+                    btnType="white"
+                    width="12rem"
+                    height="2rem"
+                    text="기부는 어떠셨나요?"
+                  />
+                )}
+              </>
+            );
+          })}
+        </CardContainer>
+      </AlignContainer>
+      <Nav />
+    </>
+  );
 };
 
 export default Schedule;
+
+const AlignContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-top: 5rem;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  width: 99vw;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  width: 99vw;
+`;
+
+const DUMMY_DATA = [
+  {
+    contractId: 14510,
+    contractDate: "2021-12-31",
+    status: "ing",
+    donationId: 2,
+    donationWishId: null,
+  },
+  {
+    contractId: 10493,
+    contractDate: "2021-03-09",
+    status: "done",
+    donationId: null,
+    donationWishId: 3,
+  },
+  {
+    contractId: 12345, // 기부 예약 식별자,
+    contractDate: "2019-12-31", // 기부 예약 일정,
+    status: "done", // 기부 예약 상태
+    donationId: 1, // 기부 예약이 '기부할래요' 예약이라면 해당 게시글의 식별자. 그렇지 않다면 null,
+    donationWishId: null, // 기부 예약이 '기부원해요' 예약이라면 해당 게시글의 식별자. 그렇지 않다면 null,
+  },
+];
