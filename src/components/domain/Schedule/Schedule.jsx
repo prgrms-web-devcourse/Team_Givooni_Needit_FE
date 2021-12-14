0087eecf3abb04245d9bde914f4d7a747c652794
gyulhana <matearz@korea.ac.kr>
2021-12-14 19:28:32 +0900

    ✨ feat: 기부 평가 emotion modal 추가

--- src/components/domain/Schedule/Schedule.jsx
+++ src/components/domain/Schedule/Schedule.jsx
@@ -2,8 +2,15 @@ import Header from "@/components/base/Header";
 import BaseButton from "@/components/base/BaseButton";
 import Nav from "@/components/base/Nav";
 import styled from "styled-components";
+import { useState } from "react";
+import {
+  Mood,
+  SentimentNeutral,
+  SentimentVeryDissatisfied,
+} from "@mui/icons-material";
 
 const Schedule = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <>
       <Header type="searchOut" fixed={true} />
@@ -16,16 +23,42 @@ const Schedule = () => {
                 <BaseButton
                   height="2rem"
                   width="6rem"
+                  style={{ fontSize: "0.8rem" }}
                   text={data.status === "done" ? "기부 완료" : "기부 진행"}
                 />
                 {data.status === "done" && (
                   <BaseButton
                     btnType="white"
-                    width="12rem"
+                    width="9rem"
                     height="2rem"
                     text="기부는 어떠셨나요?"
+                    style={{ fontSize: "0.8rem" }}
+                    onClick={() => setVisible(true)}
                   />
                 )}
+                <ModalDim style={{ display: visible ? "block" : "none" }}>
+                  <ModalContainer>
+                    <button
+                      style={{
+                        position: "relative",
+                        left: "8rem",
+                        backgroundColor: "transparent",
+                        border: "none",
+                      }}
+                      onClick={() => setVisible(false)}
+                    >
+                      X
+                    </button>
+                    <div>해당 기부 경험은 어떠셨나요?</div>
+                    <MoodContainer>
+                      <Mood onClick={() => setVisible(false)} />
+                      <SentimentNeutral onClick={() => setVisible(false)} />
+                      <SentimentVeryDissatisfied
+                        onClick={() => setVisible(false)}
+                      />
+                    </MoodContainer>
+                  </ModalContainer>
+                </ModalDim>
               </>
             );
           })}
@@ -55,6 +88,39 @@ const CardContainer = styled.div`
   padding: 0.5rem;
   box-sizing: border-box;
   width: 99vw;
+`;
+
+const ModalDim = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+`;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 1rem;
+  border-radius: 12.8px;
+  background-color: white;
+  box-shadow: 0 3px 18px rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  width: 80vw;
+  align-items: center;
+`;
+
+const MoodContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
 `;
 
 const DUMMY_DATA = [
