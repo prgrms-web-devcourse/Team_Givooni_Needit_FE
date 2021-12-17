fdd7fbc16ffb951cc2137f3d963627d3616f54fe
gyulhana <matearz@korea.ac.kr>
2021-12-17 23:09:25 +0900

    ✨ feat: axios interceptor 및 patch request 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,36 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
+const access_token = localStorage.getItem("needit_access_token");
+
+const instance = axios.create({
+  timeout: 3000,
+  headers: {
+    Authorization: access_token,
+  },
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 const getRequest = async (url = "", options = {}) => {
-  const returnResult = await axios({
+  const returnResult = await instance({
     method: "get",
     url: `${API_BASE_URL}/${url}`,
     ...options,
@@ -16,7 +44,7 @@ const getRequest = async (url = "", options = {}) => {
 };
 
 const postRequest = async (url = "", options = {}) => {
-  const returnResult = await axios({
+  const returnResult = await instance({
     method: "post",
     url: `${API_BASE_URL}/${url}`,
     ...options,
@@ -30,7 +58,7 @@ const postRequest = async (url = "", options = {}) => {
 };
 
 const putRequest = async (url = "", options = {}) => {
-  const returnResult = await axios({
+  const returnResult = await instance({
     method: "put",
     url: `${API_BASE_URL}/${url}`,
     ...options,
@@ -44,7 +72,7 @@ const putRequest = async (url = "", options = {}) => {
 };
 
 const deleteRequest = async (url = "", options = {}) => {
-  const returnResult = await axios({
+  const returnResult = await instance({
     method: "delete",
     url: `${API_BASE_URL}/${url}`,
     ...options,
@@ -57,4 +85,18 @@ const deleteRequest = async (url = "", options = {}) => {
   return returnResult;
 };
 
-export { getRequest, postRequest, putRequest, deleteRequest };
+const patchRequest = async (url = "", options = {}) => {
+  const returnResult = await instance({
+    method: "patch",
+    url: `${API_BASE_URL}/${url}`,
+    ...options,
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return returnResult;
+};
+
+export { getRequest, postRequest, putRequest, deleteRequest, patchRequest };
