9ab0511b4c174b5c07195bcfb5e5b52eaf59bcbd
khw970421 <khw970421@kakao.com>
2021-12-22 16:42:54 +0900

    ✨ feat : Message페이지 Loading 구현

--- src/pages/MessagePage.jsx
+++ src/pages/MessagePage.jsx
@@ -2,6 +2,7 @@ import Header from "@/components/base/Header";
 import MessageDetail from "@/components/domain/Message/MessageDetail";
 import Message from "@/components/domain/Message/Message";
 import Nav from "@/components/base/Nav";
+import LoadingCircular from "@/components/base/LoadingCircular";
 import { Box, Typography } from "@mui/material";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
@@ -20,6 +21,8 @@ const MessagePage = () => {
   const [userList, setUserList] = useState([]);
   const [messageList, setMessageList] = useState([]);
   const { postId, postType, receiverId } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+
   const socket = new SockJS(`${API_BASE_URL}/stomp-handshake`);
   const stompClient = Stomp.over(socket);
   const updateList = async () => {
@@ -33,22 +36,27 @@ const MessagePage = () => {
       });
     });
   });
+
   useEffect(() => {
     window.scroll({
       top: 10000000,
       behavior: "smooth",
     });
   }, [userList, messageList]);
+
   useEffect(async () => {
     const nextList = await getUserList();
     setUserList(nextList);
     if (postId) {
-      updateList();
+      await updateList();
     }
+    setIsLoading(true);
   }, [postId]);
+
   useEffect(() => {
     connect();
   }, []);
+
   const sendMessage = (content, postId, postType, receiverId) => {
     stompClient.send(
       "/app/chat",
@@ -64,33 +72,51 @@ const MessagePage = () => {
   return (
     <>
       <Header type="main" />
-      {postId ? (
-        <MessageDetail
-          list={messageList}
-          sendMessage={async (text) => {
-            await sendMessage(text, postId, postType, receiverId);
-            const nextList = await getMessageList(postId, postType, receiverId);
-            setMessageList(nextList);
-          }}
-          reserveDonation={async (date) => {
-            await createContract(date, postId, postType, receiverId);
-            const nextList = await getMessageList(postId, postType, receiverId);
-            setMessageList(nextList);
-          }}
-          contract={async (id, contractStatus) => {
-            await patchContract(id, contractStatus);
-            const nextList = await getMessageList(postId, postType, receiverId);
-            setMessageList(nextList);
-          }}
-        />
-      ) : userList.length === 0 ? (
-        <Box sx={{ display: "flex", width: "100%", justifyContent: "center" }}>
-          <Typography color="primary" variant="subtitle1" marginTop="16px">
-            주고 받은 메시지가 없습니다
-          </Typography>
-        </Box>
+      {isLoading ? (
+        postId ? (
+          <MessageDetail
+            list={messageList}
+            sendMessage={async (text) => {
+              await sendMessage(text, postId, postType, receiverId);
+              const nextList = await getMessageList(
+                postId,
+                postType,
+                receiverId
+              );
+              setMessageList(nextList);
+            }}
+            reserveDonation={async (date) => {
+              await createContract(date, postId, postType, receiverId);
+              const nextList = await getMessageList(
+                postId,
+                postType,
+                receiverId
+              );
+              setMessageList(nextList);
+            }}
+            contract={async (id, contractStatus) => {
+              await patchContract(id, contractStatus);
+              const nextList = await getMessageList(
+                postId,
+                postType,
+                receiverId
+              );
+              setMessageList(nextList);
+            }}
+          />
+        ) : userList.length === 0 ? (
+          <Box
+            sx={{ display: "flex", width: "100%", justifyContent: "center" }}
+          >
+            <Typography color="primary" variant="subtitle1" marginTop="16px">
+              주고 받은 메시지가 없습니다
+            </Typography>
+          </Box>
+        ) : (
+          <Message list={userList} />
+        )
       ) : (
-        <Message list={userList} />
+        <LoadingCircular></LoadingCircular>
       )}
 
       {postId ? "" : <Nav />}
